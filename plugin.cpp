1fce8784ce984fe8b74c9dcd9ad7b4506887fe3c
EthanPadden <e.padden1@nuigalway.ie>
2018-08-13 09:57:21 +0100

    Refactor and tidy code

--- plugin.cpp
+++ plugin.cpp
@@ -19,30 +19,24 @@ bool MinimalModel::algorithmStep()
     nextStates.reserve(nodes().size());
 
     for (Node node : nodes()) {
-        if(node.id() == 0){ // Edge case 1
+        bool left_cell, right_cell;
+        if (node.id() == 0) { // Edge case 1
             // Toroidal
-            bool left_cell = nodes().at(nodes().size()-1).attr(m_stateAttrId).toBool();
-            bool right_cell = nodes().at(1).attr(m_stateAttrId).toBool();
-            bool central_cell = node.attr(m_stateAttrId).toBool();
-
-            bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
-            nextStates.emplace_back(central_cell_next_state);
-        } else if(node.id() == (nodes().size()-1)){
-            bool left_cell = nodes().at(nodes().size()-2).attr(m_stateAttrId).toBool();
-            bool right_cell = nodes().at(0).attr(m_stateAttrId).toBool();
-            bool central_cell = node.attr(m_stateAttrId).toBool();
-
-            bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
-            nextStates.emplace_back(central_cell_next_state);
-    }else {
+            left_cell = nodes().at(nodes().size()-1).attr(m_stateAttrId).toBool();
+            right_cell = nodes().at(1).attr(m_stateAttrId).toBool();
+        } else if (node.id() == (nodes().size()-1)) {
+            left_cell = nodes().at(nodes().size()-2).attr(m_stateAttrId).toBool();
+            right_cell = nodes().at(0).attr(m_stateAttrId).toBool();
+        } else {
 //            left_cell XOR (central_cell OR right_cell)
-            bool left_cell = nodes().at(node.id()-1).attr(m_stateAttrId).toBool();
-            bool right_cell = nodes().at(node.id()+1).attr(m_stateAttrId).toBool();
-            bool central_cell = node.attr(m_stateAttrId).toBool();
-
-            bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
-            nextStates.emplace_back(central_cell_next_state);
+            left_cell = nodes().at(node.id()-1).attr(m_stateAttrId).toBool();
+            right_cell = nodes().at(node.id()+1).attr(m_stateAttrId).toBool();
         }
+
+        bool central_cell = node.attr(m_stateAttrId).toBool();
+
+        bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
+        nextStates.emplace_back(central_cell_next_state);
     }
     return true;
 }

645d2f3fb3329f9e581a61cc0c98b0a02ad3b55f
EthanPadden <e.padden1@nuigalway.ie>
2018-08-13 09:53:47 +0100

    Add logic to handle toroidal edge cases

--- plugin.cpp
+++ plugin.cpp
@@ -19,11 +19,29 @@ bool MinimalModel::algorithmStep()
     nextStates.reserve(nodes().size());
 
     for (Node node : nodes()) {
-        if(node.id() == 0 || node.id() == (nodes().size()-1)){ // Edge cases
-            continue;
-        } else {
+        if(node.id() == 0){ // Edge case 1
+            // Toroidal
+            bool left_cell = nodes().at(nodes().size()-1).attr(m_stateAttrId).toBool();
+            bool right_cell = nodes().at(1).attr(m_stateAttrId).toBool();
+            bool central_cell = node.attr(m_stateAttrId).toBool();
+
+            bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
+            nextStates.emplace_back(central_cell_next_state);
+        } else if(node.id() == (nodes().size()-1)){
+            bool left_cell = nodes().at(nodes().size()-2).attr(m_stateAttrId).toBool();
+            bool right_cell = nodes().at(0).attr(m_stateAttrId).toBool();
+            bool central_cell = node.attr(m_stateAttrId).toBool();
+
+            bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
+            nextStates.emplace_back(central_cell_next_state);
+    }else {
 //            left_cell XOR (central_cell OR right_cell)
-//            bool left_cell = nodes.at(node.id()-1).attr(m_stateAttrId).toBool();
+            bool left_cell = nodes().at(node.id()-1).attr(m_stateAttrId).toBool();
+            bool right_cell = nodes().at(node.id()+1).attr(m_stateAttrId).toBool();
+            bool central_cell = node.attr(m_stateAttrId).toBool();
+
+            bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
+            nextStates.emplace_back(central_cell_next_state);
         }
     }
     return true;

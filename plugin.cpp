62f903f5fd5f7ee4b95c301b9c35ddd11487a6c5
EthanPadden <e.padden1@nuigalway.ie>
2018-08-13 10:45:08 +0100

    Add descriptive comments

--- plugin.cpp
+++ plugin.cpp
@@ -9,8 +9,12 @@ namespace evoplex {
 
 bool MinimalModel::init()
 {
+    // gets the id of the `state` node's attribute, which is the same for all nodes
     m_stateAttrId = node(0).attrs().indexOf("state");
+
+    // initializing model attributes, which is constant throughout the simulation
     m_toroidal = attr("toroidal").toBool();
+    // if the structure is toroidal, this attribute is irrelevant
     m_edgeCaseValue = attr("edgeCaseValue").toBool();
 
     return m_stateAttrId >= 0;
@@ -23,29 +27,31 @@ bool MinimalModel::algorithmStep()
 
     for (Node node : nodes()) {
         bool left_cell, right_cell;
-        if (node.id() == 0) { // Edge case 1
-            // Toroidal
-            if(m_toroidal){
-                left_cell = nodes().at(nodes().size()-1).attr(m_stateAttrId).toBool();
+        if (node.id() == 0) { // Edge case 1: first cell
+            if (m_toroidal) {
+                left_cell = nodes().at(nodes().size()-1).attr(m_stateAttrId).toBool(); // the left cell is the last cell
             } else {
-                left_cell = m_edgeCaseValue;
+                left_cell = m_edgeCaseValue; // the left cell is the edge case value
             }
+
             right_cell = nodes().at(1).attr(m_stateAttrId).toBool();
-        } else if (node.id() == (nodes().size()-1)) {
-            left_cell = nodes().at(nodes().size()-2).attr(m_stateAttrId).toBool();
-            if(m_toroidal){
-                right_cell = nodes().at(0).attr(m_stateAttrId).toBool();
+        } else if (node.id() == (nodes().size()-1)) { // Edge case 2: last cell
+            if (m_toroidal) {
+                right_cell = nodes().at(0).attr(m_stateAttrId).toBool(); // the right cell is the first cell
             } else {
-                right_cell = m_edgeCaseValue;
+                right_cell = m_edgeCaseValue; // the right cell is the edge case value
             }
+
+            left_cell = nodes().at(nodes().size()-2).attr(m_stateAttrId).toBool();
         } else {
-//            left_cell XOR (central_cell OR right_cell)
+            // For all other cells
             left_cell = nodes().at(node.id()-1).attr(m_stateAttrId).toBool();
             right_cell = nodes().at(node.id()+1).attr(m_stateAttrId).toBool();
         }
 
         bool central_cell = node.attr(m_stateAttrId).toBool();
 
+        // next state of central cell = [left_cell XOR (central_cell OR right_cell)]
         bool central_cell_next_state = left_cell ^ (central_cell || right_cell);
         nextStates.emplace_back(central_cell_next_state);
     }
